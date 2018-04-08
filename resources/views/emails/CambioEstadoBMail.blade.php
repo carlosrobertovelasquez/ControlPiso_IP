8d61f77f8d26ea8869b0b21b85519e516a1825f4
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2018-04-08 13:45:41 -0600

    IIi

--- resources/views/emails/CambioEstadoBMail.blade.php
+++ resources/views/emails/CambioEstadoBMail.blade.php
@@ -1,14 +1,12 @@
-<!doctype html>
-<html lang="es">
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
-        <title>Requerimiento de Compras</title>
-        
-    </head>
-    <body>
-        <p> Se ha Registrado una Nuevo requerimiento de Compras para su analisis </p>
-       
-    </body>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>aqui estamos</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
 </html>
